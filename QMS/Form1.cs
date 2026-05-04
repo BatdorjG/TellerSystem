0c02b2d20b844ee6315255ba70af9ef8a85ed785
BatdorjG <batdorjganbold564@gmail.com>
2026-05-04 11:25:52 +0800

    Made QMS take queue take queue from webserver's QueueService

--- QMS/Form1.cs
+++ QMS/Form1.cs
@@ -7,56 +7,76 @@ namespace QMS
 {
     public partial class Form1 : Form
     {
-        private TempQueue tempQueue = new TempQueue();
-
+        private static readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:5062/")
+        };
         public Form1()
         {
             QuestPDF.Settings.License = LicenseType.Community;
             InitializeComponent();
         }
 
-        private byte[] GenerateValue()
+        private async Task<byte[]> GenerateValue()
         {
-            var value = tempQueue.Give();
-
-            var image = new Bitmap (200, 100);
-
-            using (var graphics = Graphics.FromImage(image))
+            try
             {
-                graphics.Clear(System.Drawing.Color.White);
-                using (var font = new Font("Arial", 48, FontStyle.Bold))
+                var response = await _httpClient.GetStringAsync("CustomerQueue");
+                if (!byte.TryParse(response, out byte value))
                 {
-                    var textSize = graphics.MeasureString(value.ToString(), font);
-                    var position = new PointF((image.Width - textSize.Width) / 2, (image.Height - textSize.Height) / 2);
-                    graphics.DrawString(value.ToString(), font, Brushes.Black, position);
+                    return null;
                 }
-            }
 
-            using (var ms = new MemoryStream())
+                var image = new Bitmap(200, 100);
+
+                using (var graphics = Graphics.FromImage(image))
+                {
+                    graphics.Clear(System.Drawing.Color.White);
+                    using (var font = new Font("Arial", 48, FontStyle.Bold))
+                    {
+                        var textSize = graphics.MeasureString(value.ToString(), font);
+                        var position = new PointF((image.Width - textSize.Width) / 2, (image.Height - textSize.Height) / 2);
+                        graphics.DrawString(value.ToString(), font, Brushes.Black, position);
+                    }
+                }
+
+                using (var ms = new MemoryStream())
+                {
+                    image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    return ms.ToArray();
+                }
+
+            }
+            catch (HttpRequestException ex)
             {
-                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                return ms.ToArray();
+                MessageBox.Show($"Error fetching value: {ex.Message}");
+                return null;
             }
-
         }
 
 
-        private void PrintButton_Click(object sender, EventArgs e)
+        private async void PrintButton_Click(object sender, EventArgs e)
         {
-            var image = GenerateValue();
+            var button = sender as Button;
+            if (button != null) { button.Enabled = false; }
+
+            var image = await GenerateValue();
 
             if (image != null) {
                 var document = Document.Create(container =>
                 {
                     container.Page(page =>
                     {
-                        page.Size(PageSizes.A8);
+                        page.Size(PageSizes.A9);
                         page.Content().AlignCenter().AlignMiddle().Image(image);
                     });
                 });
 
                 document.GeneratePdf("output.pdf");
             }
+
+            if (button != null) { button.Enabled = true; }
+
         }
     }
 }
