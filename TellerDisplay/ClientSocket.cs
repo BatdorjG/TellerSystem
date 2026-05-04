fc53c3673afca2e8f7a50a4863a0d7e75ddf2036
BatdorjG <batdorjganbold564@gmail.com>
2026-05-04 12:00:33 +0800

    Started NextCustomer endpoint for WebServer plus added teller display project that would get number from server and show that number using socket server *Havent finished yet

---
+++ TellerDisplay/ClientSocket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace TellerDisplay;
+public class ClientSocket
+{
+    public ClientSocket()
+    {
+        using Socket client = new();
+    }
+}
---
+++ TellerDisplay/Form1.cs
@@ -0,0 +1,10 @@
+using System.Net.Sockets;
+
+namespace TellerDisplay;
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
